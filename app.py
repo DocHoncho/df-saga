afe5cd5567bc5b6b87afb1747a300afefbf162d8
Joel Madigan <dochoncho@gmail.com>
2014-07-06 11:33:37 -0400

    Deleted useless files

--- app.py
+++
@@ -1,3 +0,0 @@
-from saga import app
-
-app.run(port=31337, debug=True)
--- tst_settings.py
+++
@@ -1,9 +0,0 @@
-from saga.core import settings
-from pprint import pprint
-
-settings.register_namespace('app')
-settings.register_namespace('something')
-
-t = settings.get_settings()
-pprint(t.__dict__())
-
